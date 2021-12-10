d848ffd8f8c0d84c0604ea77ff2e244d80f3137b
VaghasiyaLV <laxmivaghasiya134@gmail.com>
2021-12-10 08:16:11 +0530

    pr_1_done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,85 @@
 
 import './App.css';
-import State from './Containers/State';
-import Country from './Containers/Country';
-import City from './Containers/City';
 
 
 
 function App() {
-  return(
+    const emp =[
+      {
+        name: "amit",
+        age: 35,
+        salary: 40000,
+        bonus: 1000,
+        status: true
+      },
+      {
+        name: "ajay",
+        age: 25,
+        salary: 38000,
+        bonus: 2000,
+        status: false
+      },
+      {
+        name: "mayur",
+        age: 23,
+        salary: 50000,
+        bonus: 500,
+        status: true
+      },
+      {
+        name: "jay",
+        age: 29,
+        salary: 35000,
+        bonus: 600,
+        status: true
+      },
+      {
+        name: "raj",
+        age: 33,
+        salary: 22000,
+        bonus: 2000,
+        status: true
+      },
+    ]
+  
+    const totalExp = 
+      emp.filter((e,i) => e.status === true)
+      .map((e , i) => e.salary + e.bonus)
+      .reduce((acc , e) => acc + e , 0)
+  
+   
+  return (
     <>
+    <table>
+      <tr>
+        <th>sr. no</th>
+        <th>name</th>
+        <th>age</th>
+        <th>salary</th>
+        <th>bonus</th>
+        <th>status</th>
+        <th>totel exp</th>
+      </tr>
 
-    <State />
-    <Country />
-    <City />
+      {
+        emp.map((e,i) => {
+          return(
+            <tr>
+              <td>{i+1}</td>
+              <td>{e.name}</td>
+              <td>{e.age}</td>
+              <td>{e.salary}</td>
+              <td>{e.bonus}</td>
+              <td>{e.status ? "true" : "false"}</td>
+              <td rowspan="5">{i === 0 ? totalExp : null}</td>
+            </tr>
+          )
+        })
+      }
+    </table>
     </>
-  )
+
+  );
 }
 
 export default App;
